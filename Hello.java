87348243215d085035d49fe522fa003a1dd32cda
Anbusaravanan Madhavan <Anbusaravanan@zkteco.in>
2022-08-05 10:21:24 +0530

    Hello world pro

--- Hello.java
+++ Hello.java
@@ -3,7 +3,15 @@ class Demo
 
     public static void main(String[]args)
     {
+        System.out.println("Anbu");
+
         System.out.println("hello-World!");
+
+        for(int i=0;i<8;i++){
+
+            System.out.println("Anbu");
+    
+           }
     }
 
 }
